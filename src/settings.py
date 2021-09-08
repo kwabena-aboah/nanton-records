2d9a67e2402c7089590c4da0e8ddd38c7fc4e051
kwabena-aboah <obed30mintah@yahoo.com>
2021-09-08 01:08:21 +0000

    make it better

--- src/settings.py
+++ src/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
