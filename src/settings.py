488b242f828fcd1ad0d44954e8d2371d2ad1ca0f
kwabena-aboah <obed30mintah@yahoo.com>
2021-09-08 00:18:25 +0000

    make it better

--- src/settings.py
+++ src/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#a-&l1+*ylc#p&2xwdsui3@c6%@ll++hrf5dz&a-5t*o3-c%0%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
